49d76b7b96c951d0f61202291eacdf2b035e8085
Abdirizak Omer <abdirizakomer188@gmail.com>
2022-01-09 14:18:29 +0300

    task-list App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Form from './components/form'
+import Header from './components/header'
+import List from './components/list';
+import React, {useState} from 'react';
 function App() {
+  
+  const [todos, setTodos] = useState([{id:0,todo:"this is a simple task"}])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <Header/>
+
+      {/* Now let's start creating the form */}
+      <Form setTodo={setTodos} len={todos.length}/>
+
+      {/* Now let's create our list component */}
+      <List todoList={todos} setTodo={setTodos}/>
+      {/* Okey now that we've styled everything let's create
+          the todo function
+          first let's make a function that allow us to add items
+          into our list
+
+          Now let's add a function that remove items form 
+          the list
+      */}
     </div>
   );
 }
